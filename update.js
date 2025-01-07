4a9262ceede80ef97dd2e4d0d983dd0c3e545187
Chan Ho Yeung <hychandp@connect.ust.hk>
2025-01-07 18:32:43 +0800

    blocked by CORS policy

--- update.js
+++ update.js
@@ -1,30 +1,30 @@
 station_list = {
 
-    "11-S":{
+    "11S":{
         "name": "11",
         "gate": "S",
-        "route-id": 2004791,
-        "route-seq": 1,
-        "stop-seq": 7,
-        "type": "minibus"
+        "route-id": "2004791",
+        "route-seq": "1",
+        "stop-seq": "7",
+        "type": "gmb"
     },
 
-    "11-M":{
+    "11M":{
         "name": "11M",
         "gate": "N",
-        "route-id": 2004825,
-        "route-seq": 1,
-        "stop-seq": 3,
-        "type": "minibus"
+        "route-id": "2004825",
+        "route-seq": "1",
+        "stop-seq": "3",
+        "type": "gmb"
     },
 
     "91":{
         "name": "91",
         "gate": "S",
         "route-id": "91",
-        "route-seq": 1,
+        "route-seq": "1",
         "stop-id": "B002CEF0DBC568F5",
-        "type": "9bus",
+        "type": "9bus"
 
     },
 
@@ -32,9 +32,9 @@ station_list = {
         "name": "91M",
         "gate": "S",
         "route-id": "91M",
-        "route-seq": 1,
+        "route-seq": "1",
         "stop-id": "B002CEF0DBC568F5",
-        "type": "9bus",
+        "type": "9bus"
 
     },
 
@@ -42,29 +42,24 @@ station_list = {
         "name": "792M",
         "gate": "N",
         "route-id": "792M",
-        "route-seq": 17,
+        "route-seq": "17",
         "stop-id": "003130",
-        "type": "ctb",
+        "type": "ctb"
 
     }
 
     }
     
-    /**3592A0182BF020C7
-     * 
-     * 
-     */
 
 
 function updateStation(){
- //https://data.etagmb.gov.hk/eta/route-stop/2004791/1/7
     for(i in station_list){
         curr = station_list[i]
 
 
         url = "";
 
-        if(curr["type"] == "minibus"){
+        if(curr["type"] == "gmb"){
             url = `https://data.etagmb.gov.hk/eta/route-stop/${curr["route-id"]}/${curr["route-seq"]}/${curr["stop-seq"]}`
         }
         else if(curr["type"] == "9bus"){
@@ -79,11 +74,20 @@ function updateStation(){
             success: (data) =>{
             eta = -1
             
-            if(curr["type"] == "minibus"){
-                eta = data["data"]["eta"][0]["diff"]
-                if(eta == 0)
-                    eta = data["data"]["eta"][1]["diff"]
+            if(curr["type"] == "gmb"){
+                eta_str = data["data"]["eta"][0]["timestamp"]
+                time_diff = Date.parse(eta_str) - new Date()
+
+                if(time_diff <= 0){
+
+                    eta_str = data["data"]["eta"][1]["timestamp"]
+                    time_diff = Date.parse(eta_str) - new Date()
+                }
+
+                eta =  Math.floor(time_diff / 60000)
+
             }
+
             else if(curr["type"] == "9bus"){
                 eta_str = data["data"][0]["eta"]
                 time_diff = Date.parse(eta_str) - new Date()
