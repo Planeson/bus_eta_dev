e2f376e3775c6889e56c896ed411aefb765b69ae
Planeson <cpkplkcy@gmail.com>
2025-03-29 01:44:36 +0800

    Added cycling
    Displayed info can now cycle

--- update.js
+++ update.js
@@ -1,4 +1,6 @@
-station_list = {
+// list of all routes to add, const and defined before runtime
+
+const station_list = {
 
     "11Y": {
         "name": "11Y",
@@ -43,6 +45,15 @@ station_list = {
         "route-id": "2004826",
         "route-seq": "1",
         "stop-seq": "8",
+        "type": "gmb"
+    },
+
+    "104Y": {
+        "name": "104Y",
+        "gate": "S",
+        "route-id": "2007200",
+        "route-seq": "1",
+        "stop-seq": "1",
         "type": "gmb"
     },
 
@@ -107,6 +118,9 @@ station_list = {
     }
 };
 
+// constantly updated list of routes, type and eta
+// can put other things here like images
+
 content = {
     "11Y": {
         url: "",
@@ -134,6 +148,12 @@ content = {
     },
 
     "11SZ": {
+        url: "",
+        type: "gmb",
+        content: "",
+    },
+
+    "104Y": {
         url: "",
         type: "gmb",
         content: "",
@@ -176,9 +196,9 @@ content = {
     }
 };
 
-function precalStation(stnList) {
-    // consider moving this out such that each route calls precal once
-    for (const [stnKey, stnAtt] of Object.entries(stnList)) {
+// generates URL for each route at initialization, saved in content
+async function precalStation(routeList) {
+    for (const [stnKey, stnAtt] of Object.entries(routeList)) {
         switch (stnAtt.type) {
             case "gmb":
                 content[stnKey].url = `https://data.etagmb.gov.hk/eta/route-stop/${stnAtt["route-id"]}/${stnAtt["route-seq"]}/${stnAtt["stop-seq"]}`;
@@ -193,48 +213,62 @@ function precalStation(stnList) {
     }
 }
 
-
+// list of what layout (combination of 6 routes/items) to use
 layoutList = {
     // works on overriding, first element overrides second and so on elements
-    // dow 0 Sun, 1 Mon
+    // date of week dow : 0 Sun, 1 Mon
 
     // midnight 2 for 11S -> CHH 0000 dept
     // midnight 1 for 11S -> HAH 0030 dept
     midnight2: {},
     midnight1: {},
-    // return peak -> 91P
-    peak: {},
+    // return peak -> 91P + 291P (lol)
+    peak: {
+        dow: [1, 2, 3, 4, 5],
+        startTime: [16, 0],
+        endTime: [18, 40],
+        layout: {
+            S1: ["91MZ", "91Z"],
+            S2: ["91PZ", "291PY"],
+            S3: ["11Z", "11Z", "104Y"],
+            N1: ["792MZ"],
+            N2: ["91MY"],
+            N3: ["11MZ", "11Y"],
+        }
+    },
     // default + fallback
     normal: {
         dow: [-1, 0, 1, 2, 3, 4, 5, 6],
         startTime: [0, 0],
         endTime: [23, 59],
         layout: {
-            S1: "91MZ",
-            S2: "91Z",
-            S3: "11Z",
-            N1: "792MZ",
-            N2: "91MY",
-            N3: "11MZ" //"11Y"], 
+            S1: ["91MZ"],
+            S2: ["91Z"],
+            S3: ["11Z", "11Z", "104Y"],
+            N1: ["792MZ"],
+            N2: ["91MY"],
+            N3: ["11MZ", "11Y"],
         }
     }
-
-
-
 }
 
+// todo: add layouts, fix seconds, move parsing out, change content[key].content to eta timestamp, then move screen update out of api calls
 
-
-function updateLayout() {
-
-    for (const [layoutKey, rtNum] of Object.entries(layoutList.normal.layout)) {
+// REWRITE
+// first put fetched data into content, then pull from content and cal on it
+async function updateLayout() {
+    for (const [layoutKey, rtList] of Object.entries(layoutList.peak.layout)) {
+        const displayTime = 3 // 2 seconds each for flipping displays
+        cycleLength = displayTime * rtList.length;
+        rtNum = rtList[(dateSecond % cycleLength) / displayTime];
         updateStation(rtNum);
         document.getElementById(layoutKey).innerText = content[rtNum].content;
-        document.getElementById(layoutKey+"Num").innerText = rtNum;
+        document.getElementById(layoutKey + "Num").innerText = rtNum;
     }
-    //document.getElementById(this).innerText = content.,)
 }
 
+// REWRITE
+// takes url and puts eta timestamp into content
 async function updateStation(routeNum) {
     try {
         const response = await fetch(content[routeNum].url);
@@ -247,13 +281,13 @@ async function updateStation(routeNum) {
             eta_str = data["data"]["eta"][0]["timestamp"]
             time_diff = Date.parse(eta_str) - new Date()
 
-            if (time_diff <= 0) {
+            if (time_diff <= 480000) {
 
                 eta_str = data["data"]["eta"][1]["timestamp"]
                 time_diff = Date.parse(eta_str) - new Date()
             }
 
-            eta = Math.floor(time_diff / 60000)
+            eta = (Math.floor(time_diff / 60000)).toString() + ":" + (Math.floor(time_diff / 1000) % 60).toString();
 
         }
 
@@ -261,26 +295,26 @@ async function updateStation(routeNum) {
             eta_str = data["data"][0]["eta"]
             time_diff = Date.parse(eta_str) - new Date()
 
-            if (time_diff <= 0) {
+            if (time_diff <= 480000) {
 
                 eta_str = data["data"][1]["eta"]
                 time_diff = Date.parse(eta_str) - new Date()
             }
 
-            eta = Math.floor(time_diff / 60000)
+            eta = (Math.floor(time_diff / 60000)).toString() + ":" + (Math.floor(time_diff / 1000) % 60).toString();
 
         }
         else if (content[routeNum]["type"] == "ctb") {
             eta_str = data["data"][0]["eta"]
             time_diff = Date.parse(eta_str) - new Date()
 
-            if (time_diff <= 0) {
+            if (time_diff <= 480000) {
 
                 eta_str = data["data"][1]["eta"]
                 time_diff = Date.parse(eta_str) - new Date()
             }
 
-            eta = Math.floor(time_diff / 60000)
+            eta = (Math.floor(time_diff / 60000)).toString() + ":" + (Math.floor(time_diff / 1000) % 60).toString();
 
         }
         content[routeNum].content = eta;
@@ -290,68 +324,18 @@ async function updateStation(routeNum) {
     }
 }
 
-function oldupdateStation() {
-    $.get({
-        url: url,
-        success: (data) => {
-            eta = "--";
-
-            if (curr["type"] == "gmb") {
-                eta_str = data["data"]["eta"][0]["timestamp"]
-                time_diff = Date.parse(eta_str) - new Date()
-
-                if (time_diff <= 0) {
-
-                    eta_str = data["data"]["eta"][1]["timestamp"]
-                    time_diff = Date.parse(eta_str) - new Date()
-                }
-
-                eta = Math.floor(time_diff / 60000)
-
-            }
-
-            else if (curr["type"] == "kmb") {
-                eta_str = data["data"][0]["eta"]
-                time_diff = Date.parse(eta_str) - new Date()
-
-                if (time_diff <= 0) {
-
-                    eta_str = data["data"][1]["eta"]
-                    time_diff = Date.parse(eta_str) - new Date()
-                }
-
-                eta = Math.floor(time_diff / 60000)
-
-            }
-            else if (curr["type"] == "ctb") {
-                eta_str = data["data"][0]["eta"]
-                time_diff = Date.parse(eta_str) - new Date()
-
-                if (time_diff <= 0) {
-
-                    eta_str = data["data"][1]["eta"]
-                    time_diff = Date.parse(eta_str) - new Date()
-                }
-
-                eta = Math.floor(time_diff / 60000)
-
-            }
-
-            $(`#${i}`).html(eta)
-            // without async, it complains CORS (cross site data transfer not same domain shit)
-        }
-    })
-    //}, async: false}) 
-}
-
-
-async function fetchTime(retries = 30, delay = 200) {
+// clock function to get internet time
+async function fetchTime(retries = 100, delay = 200) {
     for (let attempt = 1; attempt <= retries; attempt++) {
         try {
-            let response = await fetch('https://worldtimeapi.org/api/timezone/Asia/Hong_Kong', { cache: "no-store" });
+            // apis unstable
+            let response = await fetch('https://www.timeapi.io/api/timezone/zone?timeZone=Asia%2FHong_Kong', { cache: "no-store" });
+            //let response = await fetch('https://worldtimeapi.org/api/timezone/Asia/Hong_Kong', { cache: "no-store" });
+
             if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
             let data = await response.json();
-            return new Date(data.utc_datetime); // Successfully fetched time
+            return new Date(data.currentLocalTime);
+            //return new Date(data.utc_datetime); // Successfully fetched time
         } catch (error) {
             console.error(`Attempt ${attempt} failed:`, error);
             if (attempt < retries) {
@@ -364,10 +348,13 @@ async function fetchTime(retries = 30, delay = 200) {
     }
 }
 
+// used to determine layout used
 dateDoW = -1;
 dateHour = -1;
 dateMinute = -1;
+dateSecond = -1;
 
+// initializes clock
 async function startClock() {
     let currentTime = await fetchTime();
     document.getElementById('timeSynced').innerText = "Last synced: " + currentTime.toLocaleTimeString();
@@ -381,12 +368,14 @@ async function startClock() {
         dateDoW = currentTime.getDay();
         dateHour = currentTime.getHours();
         dateMinute = currentTime.getMinutes();
+        dateSecond = currentTime.getSeconds();
     }
 
     // Align first update with the next full second
     let msToNextSecond = 1000 - (currentTime.getMilliseconds());
     setTimeout(() => {
         updateClock(); // First update precisely at the next full second
+        setInterval(updateLayout, 1000);
         setInterval(updateClock, 1000);
     }, msToNextSecond);
 
@@ -399,12 +388,8 @@ async function startClock() {
     }, 60 * 60 * 1000); // 60 minutes * 60 seconds * 1000 ms
 }
 
-
+// initialization
 $(document).ready(() => {
-    //precalStation(station_list);
-    //setInterval(precalStation(station_list),18000000);
-    //updateStation();
+    startClock();
     precalStation(station_list);
-    setInterval(updateLayout, 5000);
-    //startClock();
 })
